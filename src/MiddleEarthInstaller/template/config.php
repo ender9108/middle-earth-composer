0d1b14e718845cb7eefba0140a09c9656c6d9c3d
Alexandre Berthelot <alexandreberthelot9108@gmail.com>
2018-01-15 16:31:01 +0100

    Update MiddleEarthInstaller

--- src/MiddleEarthInstaller/template/config.php
+++ src/MiddleEarthInstaller/template/config.php
@@ -4,6 +4,5 @@ use EnderLab\MiddleEarth\Application\App;
 return [
     'app.env'                   => \DI\env('ENV', App::ENV_PROD),
     'app.cache.path'            => 'tmp/cache',
-    'app.enableErrorHandler'    => \DI\env('ERROR', true),
-    'app.container.cache'       => ''
+    'app.enableErrorHandler'    => \DI\env('ERROR', true)
 ];
